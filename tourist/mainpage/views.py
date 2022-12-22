b464909bbc5b09284fd0584225efe7531c552ba4
Ilya <gijhbv@mail.ru>
2022-12-22 20:05:18 +0300

    le,km

--- tourist/mainpage/views.py
+++ tourist/mainpage/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {'Zach' : "Диплом", 'Zach2' : "Зачет"}
+    context = {}
     return render(
         request,              # Запрос
 	'mainpage/index.html', # путь к шаблону
