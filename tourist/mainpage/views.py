171ed36e2d0ceb955d2f67aa35ae6e48163c4e8a
GurIlVik <gilvik1@yandex.ru>
2022-12-27 16:28:13 +0300

    на проверку

--- tourist/mainpage/views.py
+++ tourist/mainpage/views.py
@@ -5,10 +5,12 @@ from django.template import loader
 # Create your views here.
 def index(request):
     #template = loader.get.template('mainpage/index.html')
-    context = {'window_height':100,
-               'window_widht':50,
-               'Zach': ";lsdfjtghiu", 
-               'list_1' : ['lsrhiu', 'laeidb', 'lseiuhgerilu']}
+    context = {
+        "an" : 'нечто',
+               'list_1' : ['главное меню пункт1', 'главное меню пункт2', 'главное меню пункт3', 'главное меню пункт4', "главное меню пункт5", "главное меню пункт6"],
+               'list_2' : ['не главное меню пункт1', 'не главное меню пункт2', 'не главное меню пункт3', 'не главное меню пункт4', 'не главное меню пункт5',],
+               'list_3' : ['второстепенное меню пункт1', 'второстепенное меню пункт2', 'второстепенное меню пункт3', 'второстепенное меню пункт4', 'второстепенное меню пункт5',]
+               }
     return render(
         request,              # Запрос
 	'mainpage/index.html', # путь к шаблону
